d87687d62e0943aca6ed23877bd59ce83a3795af
Annyce Davis <annyce.davis@gmail.com>
2021-05-27 12:45:54 -0400

    [+] Stored a file in internal storage

--- app/src/main/java/com/example/myapplication/data/ProductRepository.kt
+++ app/src/main/java/com/example/myapplication/data/ProductRepository.kt
@@ -4,9 +4,11 @@ import android.app.Application
 import android.util.Log
 import com.example.myapplication.LOG_TAG
 import com.squareup.moshi.Moshi
+import com.squareup.moshi.Types
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
+import java.io.File
 
 const val BASE_ENDPOINT_URL = "https://2873199.youcanlearnit.net/"
 
@@ -31,9 +33,21 @@ class ProductRepository(private val app: Application) {
         val response = productApi.getProducts()
         return if (response.isSuccessful) {
             Log.i(LOG_TAG, "loaded from webservice")
-            response.body() ?: emptyList()
+            val products = response.body() ?: emptyList()
+            storeDataInFile(products)
+
+            products
         } else
             emptyList()
+    }
+
+    private fun storeDataInFile(products: List<Product>?) {
+        val listType = Types.newParameterizedType(List::class.java, Product::class.java)
+        val fileContents = moshi.adapter<List<Product>>(listType)
+            .toJson(products)
+
+        val file = File(app.cacheDir, "products.json")
+        file.writeText(fileContents)
     }
 
 }

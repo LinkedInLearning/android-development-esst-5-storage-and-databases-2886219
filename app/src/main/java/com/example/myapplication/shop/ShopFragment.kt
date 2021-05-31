dd536023f89f1de4c0b0b247899db36f222a9543
Annyce Davis <annyce.davis@gmail.com>
2021-05-31 15:35:11 -0400

    [+] Added request for write external storage permission

--- app/src/main/java/com/example/myapplication/shop/ShopFragment.kt
+++ app/src/main/java/com/example/myapplication/shop/ShopFragment.kt
@@ -1,10 +1,14 @@
 package com.example.myapplication.shop
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
@@ -13,12 +17,26 @@ import com.example.myapplication.R
 import com.example.myapplication.SharedViewModel
 import com.example.myapplication.data.Product
 import com.example.myapplication.databinding.FragmentShopBinding
+import com.google.android.material.snackbar.Snackbar
 
 class ShopFragment : Fragment() {
 
     private val viewModel: SharedViewModel by activityViewModels()
     private var _binding: FragmentShopBinding? = null
     private val binding get() = _binding!!
+
+    private val requestPermissionLauncher =
+        registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) { isGranted: Boolean ->
+            if (isGranted) {
+                Snackbar.make(
+                    binding.root,
+                    R.string.storage_permission_granted,
+                    Snackbar.LENGTH_LONG
+                ).show()
+            }
+        }
 
     private val onItemClick: (Product) -> Unit = { product ->
         Log.i(LOG_TAG, "the selected product: $product")
@@ -37,6 +55,8 @@ class ShopFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        checkStoragePermission()
+
         viewModel.products.observe(viewLifecycleOwner, { products ->
             binding.productList.adapter = ProductAdapter(products, onItemClick)
         })
@@ -45,6 +65,17 @@ class ShopFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun checkStoragePermission() {
+        if (ContextCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+        ) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissionLauncher.launch(
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+            )
+        }
     }
 
 }

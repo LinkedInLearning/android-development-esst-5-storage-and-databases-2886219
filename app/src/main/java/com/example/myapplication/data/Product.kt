bccd48f99cc50c8788852654682fe4a46454032f
Annyce Davis <annyce.davis@gmail.com>
2021-07-09 12:55:05 -0400

    [+] Created Room Entity and Database

--- app/src/main/java/com/example/myapplication/data/Product.kt
+++ app/src/main/java/com/example/myapplication/data/Product.kt
@@ -1,8 +1,13 @@
 package com.example.myapplication.data
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.squareup.moshi.Json
 
+@Entity(tableName = "products")
 data class Product(
+        @PrimaryKey(autoGenerate = true) val id: Int = 0,
+        var quantity: Int = 0,
         @Json(name = "productName") val name: String,
         val imageFile: String,
         val description: String,
---
+++ app/src/main/java/com/example/myapplication/data/ProductDatabase.kt
@@ -0,0 +1,28 @@
+package com.example.myapplication.data
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [Product::class], version = 1)
+abstract class ProductDatabase : RoomDatabase() {
+
+    companion object {
+        @Volatile
+        private var INSTANCE: ProductDatabase? = null
+
+        fun getDatabase(context: Context): ProductDatabase {
+            if (INSTANCE == null) {
+                synchronized(this) {
+                    INSTANCE = Room.databaseBuilder(
+                        context,
+                        ProductDatabase::class.java,
+                        "two-trees-oil.db"
+                    ).build()
+                }
+            }
+            return INSTANCE!!
+        }
+    }
+}
